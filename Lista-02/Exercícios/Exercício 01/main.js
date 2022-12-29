ad27889f7abaaefe19fc41860118443d50e87017
HidekiSanches <thiago.sanches@sptech.school>
2022-12-29 04:59:23 -0300

    Adição de comentário

--- Lista-02/Exercícios/Exercício 01/main.js
+++ Lista-02/Exercícios/Exercício 01/main.js
@@ -13,22 +13,3 @@
 // ░▒░░▒░░░█▓▓▓█▓▓▓█▓▓▓▓█ 
 // ░▒░░▒░░░█▓▓▓█░░░█▓▓▓█ 
 // ░▒░░▒░░██▓██░░░██▓▓██
-
-function pare() {
-    error.innerHTML = `<br>Pare e aguarde!`
-    setTimeout(()=> {
-        error.innerHTML = ``
-    }, "2000")
-}
-function pense() {
-    error.innerHTML = `<br>Pense se pode seguir ou se deve parar!`
-    setTimeout(()=> {
-        error.innerHTML = ``
-    }, "2000")
-}
-function siga() {
-    error.innerHTML = `<br>Pode seguir adiante :)`
-    setTimeout(()=> {
-        error.innerHTML = ``
-    }, "2000")
-}
